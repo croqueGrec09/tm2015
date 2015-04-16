1fa7d56bfb72d04016b569816996c9659e02158d
claesn <c.neuefeind@uni-koeln.de>
2015-04-16 11:45:39 +0200

    Add package 'web' (Crawler, WebDocument)

--- tm2015/src/de/uni_koeln/spinfo/textengineering/TestSuiteIR.java
+++ tm2015/src/de/uni_koeln/spinfo/textengineering/TestSuiteIR.java
@@ -8,6 +8,7 @@ import de.uni_koeln.spinfo.textengineering.ir.boole.TestBooleanIR;
 import de.uni_koeln.spinfo.textengineering.ir.evaluation.TestEval;
 import de.uni_koeln.spinfo.textengineering.ir.lucene.TestLucene;
 import de.uni_koeln.spinfo.textengineering.ir.ranked.TestRankedIR;
+import de.uni_koeln.spinfo.textengineering.ir.web.TestWebIR;
 
 /*
  * Eine Suite ermöglicht das ausführen von mehreren Klassen mit Tests. Wird ausgefuehrt mit Run As
@@ -19,6 +20,6 @@ import de.uni_koeln.spinfo.textengineering.ir.ranked.TestRankedIR;
  * @author Fabian Steeg, Claes Neuefeind (c.neuefeind@uni-koeln.de)
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ TestBasicIR.class, TestBooleanIR.class, TestRankedIR.class, TestEval.class, TestLucene.class})
+@Suite.SuiteClasses({ TestBasicIR.class, TestBooleanIR.class, TestRankedIR.class, TestEval.class, TestLucene.class, TestWebIR.class})
 public class TestSuiteIR {
 }
